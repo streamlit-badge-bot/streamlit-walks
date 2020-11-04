ad8d7d1c23c6e788adb273a088135d6d41236cf9
Pip <70948349+pippinstall@users.noreply.github.com>
2020-11-04 16:01:38 +0000

    Update App_1.py

--- App_1.py
+++ App_1.py
@@ -5,11 +5,11 @@ import numpy as np
 # Title of app
 st.title('Wainwrights')
 
-# url = 'https://en.wikipedia.org/wiki/List_of_Wainwrights'
-# html = pd.read_html(url, index_col=0)
-# df = html[1]
+url = 'https://en.wikipedia.org/wiki/List_of_Wainwrights'
+html = pd.read_html(url, index_col=0)
+df = html[1]
+st.write(df)
 
-# display(df)
 # print(df.columns.values)
 
 # df.drop(columns = ['Section', 'Birkett', 'Prom. (m)', 'Prom. (ft)', 'Classification(§\xa0DoBIH codes)'])
