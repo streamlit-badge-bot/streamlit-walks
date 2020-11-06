9551c646b957a87164052a273adc168930610a6c
Pip <70948349+pippinstall@users.noreply.github.com>
2020-11-06 15:42:56 +0000

    Update App_1.py

--- App_1.py
+++ App_1.py
@@ -17,6 +17,6 @@ def get_data():
     return df
 df = get_data()
 
-cm = sns.light_palette("grey", as_cmap=True)
+cm = sns.light_palette("blue", as_cmap=True)
 s = df.style.background_gradient(cmap=cm)
 s
