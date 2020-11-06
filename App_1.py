f73efa4b05084d2db4a35b147b6bc72033a82156
Pip <70948349+pippinstall@users.noreply.github.com>
2020-11-06 14:44:52 +0000

    Update App_1.py

--- App_1.py
+++ App_1.py
@@ -1,15 +1,15 @@
 import streamlit as st
 import pandas as pd 
-import numpy as np
-from lxml import etree as et
+# import numpy as np
+# from lxml import etree as et
 
 # Title of app
 st.title('Wainwrights')
 
 url = 'https://en.wikipedia.org/wiki/List_of_Wainwrights'
 html = pd.read_html(url, index_col=0)
-df = html[1]
-st.write(df)
+# df = html[1]
+# st.write(df)
 
 # print(df.columns.values)
 
