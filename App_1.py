12075f7fed59aee691c9218944eb73c5ea5224b8
Pip <70948349+pippinstall@users.noreply.github.com>
2020-11-06 14:59:27 +0000

    Update App_1.py

--- App_1.py
+++ App_1.py
@@ -1,11 +1,5 @@
 import pandas as pd
 
-@st.cache
-def load_data():
-    url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
-    html = pd.read_html(url, header = 0)
-    df = html[0]
-    return df
-
-df = load_data()
-sector = df.groupby('GICS Sector')
+url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+html = pd.read_html(url, header = 0)
+df = html[0]
