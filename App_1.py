7f7ea294a143dbd4e5f6da1e37882d325f7f88d7
Pip <70948349+pippinstall@users.noreply.github.com>
2020-11-06 15:10:52 +0000

    Update App_1.py

--- App_1.py
+++ App_1.py
@@ -1,5 +1,10 @@
-import pandas as pd
+import streamlit as st
+import pandas as pd 
+import numpy as np
 
-url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
-html = pd.read_html(url, header = 0)
-df = html[0]
+url = 'https://en.wikipedia.org/wiki/List_of_Wainwrights'
+html = pd.read_html(url, index_col=0)
+df = html[1]
+
+display(df)
+print(df.columns.values)
