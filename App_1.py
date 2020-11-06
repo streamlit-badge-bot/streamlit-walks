6fd61fa6267cd7571f9f9aa8f7c5801aaf77a6ba
Pip <70948349+pippinstall@users.noreply.github.com>
2020-11-06 15:35:43 +0000

    Update App_1.py

--- App_1.py
+++ App_1.py
@@ -4,12 +4,12 @@ import numpy as np
 
 st.title('Wainwrights: Which one should I climb?')
 
-st.markdown("First we will import the Wainwrights, their geographical location, and their heights below.")
-url = "https://en.wikipedia.org/wiki/List_of_Wainwrights"
-html = pd.read_html(url, index_col=0)
-df = html[1]
+# st.markdown("First we will import the Wainwrights, their geographical location, and their heights below.")
+# url = "https://en.wikipedia.org/wiki/List_of_Wainwrights"
+# html = pd.read_html(url, index_col=0)
+# df = html[1]
 
-df
+# df
 
 
 @st.cache
