af7df2e5e20df317ff9b66fc38c23428dffd0e5f
Pip <70948349+pippinstall@users.noreply.github.com>
2020-11-06 15:33:51 +0000

    Update App_1.py

--- App_1.py
+++ App_1.py
@@ -5,6 +5,9 @@ import numpy as np
 st.title('Wainwrights: Which one should I climb?')
 st.markdown("Welcome to this in-depth introduction to [...].")
 
+st.markdown("First we will import the Wainwrights, their geographical location, and their heights below.")
 url = "https://en.wikipedia.org/wiki/List_of_Wainwrights"
-df =  pd.read_html(url, index_col=0)
+html = pd.read_html(url, index_col=0)
+df = html[1]
+
 df
