fa610f050b6d71ee57b24484e00572c686afaaab
Pip <70948349+pippinstall@users.noreply.github.com>
2020-11-06 15:30:07 +0000

    Update App_1.py

--- App_1.py
+++ App_1.py
@@ -6,7 +6,7 @@ st.title('Wainwrights: Which one should I climb?')
 st.markdown("Welcome to this in-depth introduction to [...].")
 
 # Cache data for future use
-@st.cache
+st.cache
 def get_data():
     url = "https://en.wikipedia.org/wiki/List_of_Wainwrights"
     return pd.read_html(url, index_col=0)
