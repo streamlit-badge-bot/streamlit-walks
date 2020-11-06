5e8720e87a85dd802dbe0bfcdd8f0aaaedb4f370
Pip <70948349+pippinstall@users.noreply.github.com>
2020-11-06 15:14:20 +0000

    Update App_1.py

--- App_1.py
+++ App_1.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd 
 import numpy as np
 
+st.title('Wainwrights')
+
 url = 'https://en.wikipedia.org/wiki/List_of_Wainwrights'
 html = pd.read_html(url, index_col=0)
 df = html[1]
