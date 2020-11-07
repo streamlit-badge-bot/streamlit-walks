0227ac660a03d4734224793ca1fb582351727fd3
Pip <70948349+pippinstall@users.noreply.github.com>
2020-11-07 16:36:37 +0000

    Update App_1.py

--- App_1.py
+++ App_1.py
@@ -35,8 +35,8 @@ df_1 = pd.DataFrame(np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4], colum
 st.pydeck_chart(pdk.Deck(
     map_style='mapbox://styles/mapbox/light-v9',
     initial_view_state = pdk.ViewState(
-        latitude = 54.27000,
-        longitude = -3.06000,
+        latitude = 54.45,
+        longitude = -3.1,
         zoom = 9,
         pitch = 50,
     ),
