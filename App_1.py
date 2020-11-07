5e183bca0a57c358a0c4c13dc71237c3f9663d63
Pip <70948349+pippinstall@users.noreply.github.com>
2020-11-07 18:29:42 +0000

    Update App_1.py

--- App_1.py
+++ App_1.py
@@ -1,3 +1,6 @@
+# --------------------------------
+# Packages
+# --------------------------------
 import streamlit as st
 import pandas as pd 
 import numpy as np
@@ -6,21 +9,17 @@ import seaborn as sns
 import pydeck as pdk
 from OSGridConverter import grid2latlong
 
+# --------------------------------
+# Title
+# --------------------------------
 st.title('Wainwrights: Which one should I climb?')
 st.markdown("Wainwrights are the 214 English peaks mapped out in Alfred Wainwright's Pictorial Guide to the Lakeland Fells (1955–66). It is popular for walkers to use these routes when climbing a fell in the Lake District. However, with so much choice, how can we narrow down which one to walk up? The purpose of this app is to narrow down the Wainwrights to aid in your next fell climb in the Lake District.")
 
+# --------------------------------
+# Import Data
+# --------------------------------
 st.header("Table of all the Wainwrights. The darker the shade of green, the taller the fell is.")
 st.markdown("All the Wainwrights have been listed below.")
-
-# Streamlit will perform internal magic so that the data will be downloaded only once and cached for future use
-# @st.cache
-# def get_data():
-#     url = "https://en.wikipedia.org/wiki/List_of_Wainwrights"
-#     html = pd.read_html(url, index_col=1)
-#     df = html[1]
-#     df = df.drop(columns = ['Height Rank', 'Birkett', 'Prom. (m)', 'Prom. (ft)', 'Classification(§\xa0DoBIH codes)'])
-#     return df
-# df = get_data()
 
 url = "https://en.wikipedia.org/wiki/List_of_Wainwrights"
 html = pd.read_html(url, index_col=1)
@@ -28,14 +27,16 @@ df = html[1]
 df = df.drop(columns = ['Height Rank', 'Birkett', 'Prom. (m)', 'Prom. (ft)', 'Classification(§\xa0DoBIH codes)'])
 df['Latitude'] = df['OS Grid Reference'].apply(lambda x: grid2latlong(x).latitude)
 df['Longitude'] = df['OS Grid Reference'].apply(lambda x: grid2latlong(x).longitude)
-
 cm = sns.light_palette("seagreen", as_cmap=True)
 st.dataframe(df.style.background_gradient(cmap=cm))
 
+# --------------------------------
+# View on a map
+# --------------------------------
 st.markdown("Lets compare the heights on an area chart.")
 st.pydeck_chart(pdk.Deck(
     
-    map_style = 'mapbox://styles/mapbox/light-v9',
+#     map_style = 'mapbox://styles/mapbox/light-v9',
     
     initial_view_state = pdk.ViewState(
         latitude = 54.45,
