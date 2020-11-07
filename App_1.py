d0c6bb31ef13f17a44f2dbaec032425e40f86aca
Pip <70948349+pippinstall@users.noreply.github.com>
2020-11-07 20:33:36 +0000

    Update App_1.py

--- App_1.py
+++ App_1.py
@@ -44,6 +44,6 @@ st.pydeck_chart(pdk.Deck(
                        data = df,
                        get_position = '[Longitude, Latitude]',
                        radius = 500,
-                       get_elevation = d => d['Hieght (m)'],
+                       get_elevation = d['Hieght (m)'],
                       )
 ))
