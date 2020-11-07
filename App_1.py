d6a63c087c23935abfbc2d6c3e0c693931dff162
Pip <70948349+pippinstall@users.noreply.github.com>
2020-11-07 19:12:08 +0000

    Update App_1.py

--- App_1.py
+++ App_1.py
@@ -42,11 +42,6 @@ st.pydeck_chart(pdk.Deck(
     layers = pdk.Layer('HexagonLayer', data = df,
 #                        colorDomain = [0, len(df)],
                        get_position = '[Longitude, Latitude]',
-                       auto_highlight = True,
-                       elevation_scale = 25,
-                       pickable = True,
-                       elevationRange = [0, 1000],
-#                        elevation_range = [min(df['Height (m)']), max(df['Height (m)'])],
-                       extruded = True,
-                       coverage = 1),
+                       elevation_range = [min(df['Height (m)']), max(df['Height (m)'])],
+                      ),
 ))
