e4eb888d63548f1a904d29f7ca6d1e0115a0df1c
Pip <70948349+pippinstall@users.noreply.github.com>
2020-11-07 15:58:35 +0000

    Update App_1.py

--- App_1.py
+++ App_1.py
@@ -27,3 +27,6 @@ st.markdown("Lets compare the heights on an area chart.")
 # df_plot = df.set_index("name",drop=True,inplace=True)
 # st.area_chart(data=df, width=0, height=0, use_container_width=True)
 st.area_chart(data=df['Height (m)'])
+
+fig = ff.create_distplot(df, group_labels, bin_size=[.1, .25, .5])
+st.plotly_chart(fig, use_container_width=True)
