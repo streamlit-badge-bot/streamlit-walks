ad49f11a6ff2ee98d870b6a765645cb679e5765d
Pip <70948349+pippinstall@users.noreply.github.com>
2020-11-07 15:59:10 +0000

    Update App_1.py

--- App_1.py
+++ App_1.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.figure_factory as ff
 
 st.title('Wainwrights: Which one should I climb?')
 st.markdown("Wainwrights are the 214 English peaks mapped out in Alfred Wainwright's Pictorial Guide to the Lakeland Fells (1955–66). It is popular for walkers to use these routes when climbing a fell in the Lake District. However, with so much choice, how can we narrow down which one to walk up? The purpose of this app is to narrow down the Wainwrights to aid in your next fell climb in the Lake District.")
