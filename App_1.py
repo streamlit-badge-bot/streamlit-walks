00cddc77afd6aceaf2dfda012e6e24258d10c452
Pip <70948349+pippinstall@users.noreply.github.com>
2020-11-07 16:43:00 +0000

    Update App_1.py

--- App_1.py
+++ App_1.py
@@ -31,32 +31,33 @@ st.markdown("Lets compare the heights on an area chart.")
 
 
 df_1 = pd.DataFrame(np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4], columns=['lat', 'lon'])
+df_1
 
 st.pydeck_chart(pdk.Deck(
-    map_style='mapbox://styles/mapbox/light-v9',
+    map_style = 'mapbox://styles/mapbox/light-v9',
+    
     initial_view_state = pdk.ViewState(
         latitude = 54.45,
         longitude = -3.1,
         zoom = 9,
-#         pitch = 50,
     ),
-    layers=[
-        pdk.Layer(
-            'HexagonLayer',
-            data=df_1,
-            get_position='[lon, lat]',
-            radius=200,
-            elevation_scale=4,
-            elevation_range=[0, 1000],
-            pickable=True,
-            extruded=True,
-        ),
-        pdk.Layer(
-            'ScatterplotLayer',
-            data=df,
-            get_position='[lon, lat]',
-            get_color='[200, 30, 0, 160]',
-            get_radius=200,
-        ),
-    ],
+    
+    layers = [pdk.Layer(
+        'HexagonLayer',
+        data = df_1,
+        get_position = '[lon, lat]',
+        radius = 200,
+        elevation_scale = 4,
+        elevation_range=[0, 1000],
+        pickable=True,
+        extruded=True,
+    ),
+              pdk.Layer(
+                  'ScatterplotLayer',
+                  data = df_1,
+                  get_position='[lon, lat]',
+                  get_color='[200, 30, 0, 160]',
+                  get_radius=200,
+              ),
+             ],
 ))
