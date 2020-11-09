eec6a5f64a5342b75dfe11d0aeb0301fb180b561
Pip <70948349+pippinstall@users.noreply.github.com>
2020-11-09 13:10:24 +0000

    Update App_1.py

--- App_1.py
+++ App_1.py
@@ -75,3 +75,24 @@ st.pydeck_chart(pdk.Deck(
         ),
     ],
 ))
+
+st.deck_gl_chart(
+    viewport={
+        'latitude': 37.76,
+        'longitude': -122.4,
+        'zoom': 11,
+        'pitch': 50,
+    },
+    layers=[{
+        'type': 'HexagonLayer',
+        'data': df,
+        'radius': 200,
+        'elevationScale': 4,
+        'elevationRange': [0, 1000],
+        'pickable': True,
+        'extruded': True,
+    }, {
+        'type': 'ScatterplotLayer',
+        'data': df,
+    }]
+)
