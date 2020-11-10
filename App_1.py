8e1143a4e01072d0f4020dd9e9c7a2ff9464ae0b
Pip <70948349+pippinstall@users.noreply.github.com>
2020-11-10 09:35:47 +0000

    Update App_1.py

--- App_1.py
+++ App_1.py
@@ -17,9 +17,13 @@ st.markdown('<style>h3{color: black; text-align:center;}</style>', unsafe_allow_
 # --------------------------------
 # Title
 # --------------------------------
-st.title("Geographic Data")
 st.title("Wainwrights: Which one should I climb?")
 st.header("Wainwrights are the 214 English peaks mapped out in Alfred Wainwright's Pictorial Guide to the Lakeland Fells (1955–66).")
+
+# --------------------------------
+# Sidebar
+# --------------------------------
+# st.sidebar.header("Filter")
 
 # --------------------------------
 # Import Data
@@ -38,10 +42,8 @@ def load_data():
 df = load_data()
 
 # --------------------------------
-# Sidebar
+# Table
 # --------------------------------
-# st.sidebar.header("Filter")
-
 cm = sns.light_palette("seagreen", as_cmap=True)
 st.dataframe(df.style.background_gradient(cmap=cm))
 
@@ -51,14 +53,6 @@ st.dataframe(df.style.background_gradient(cmap=cm))
 st.markdown("Lets compare the heights on an area chart.")
 
 # st.map(df)
-
-lat_lon = pd.DataFrame(df)
-lat_lon = lat_lon.drop(columns = ['Height Rank', 'Name', 'Section', 'Height (m)'])
-lat_lon
-
-df_1 = pd.DataFrame(np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4], columns=['lat', 'lon'])
-df_1
-
 
 midpoint = (np.average(lat_lon['lat']), np.average(lat_lon['lon']))
 
@@ -70,24 +64,16 @@ st.pydeck_chart(pdk.Deck(
         zoom = 11,
         pitch = 50,
     ),
-    layers=[
-        pdk.Layer(
-            'HexagonLayer',
-            data = lat_lon,
-            get_position = '[lon, lat]',
-            radius = 200,
-            elevation_scale = 40,
-            elevation_range = [0, 214],
-            pickable = True,
-            extruded = True,
-            get_color = '[200, 30, 0, 160]',
-        ),
-#         pdk.Layer(
-#             'ScatterplotLayer',
-#             data = lat_lon,
-#             get_position = '[Longitude, Latitude]',
-#             get_color = '[200, 30, 0, 160]',
-#             get_radius = 200,
-#         ),
-    ],
+    layers=
+    pdk.Layer(
+        'HexagonLayer',
+        data = lat_lon,
+        get_position = '[lon, lat]',
+        radius = 200,
+        elevation_scale = 40,
+        elevation_range = [0, 214],
+        pickable = True,
+        extruded = True,
+        get_color = '[200, 30, 0, 160]',
+    ),
 ))
