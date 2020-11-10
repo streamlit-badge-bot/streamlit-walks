b585a2fba8ad0ca6f819d07b958d6e5848994db9
Pip <70948349+pippinstall@users.noreply.github.com>
2020-11-10 08:51:44 +0000

    Update App_1.py

--- App_1.py
+++ App_1.py
@@ -47,11 +47,13 @@ st.map(df)
 df = pd.DataFrame(np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4], columns=['lat', 'lon'])
 df
 
+midpoint = (np.average(df["Latitude"]), np.average(df["Longitude"]))
+
 st.pydeck_chart(pdk.Deck(
     map_style='mapbox://styles/mapbox/light-v9',
     initial_view_state=pdk.ViewState(
-        latitude=37.76,
-        longitude=-122.4,
+        latitude=midpoint[0],
+        longitude=midpoint[1],
         zoom=11,
         pitch=50,
     ),
@@ -59,7 +61,7 @@ st.pydeck_chart(pdk.Deck(
         pdk.Layer(
             'HexagonLayer',
             data=df,
-            get_position='[lon, lat]',
+            get_position='[Longitude, Latitude]',
             radius=200,
             elevation_scale=4,
             elevation_range=[0, 1000],
@@ -69,30 +71,9 @@ st.pydeck_chart(pdk.Deck(
         pdk.Layer(
             'ScatterplotLayer',
             data=df,
-            get_position='[lon, lat]',
+            get_position='[Longitude, Latitude]',
             get_color='[200, 30, 0, 160]',
             get_radius=200,
         ),
     ],
 ))
-
-st.deck_gl_chart(
-    viewport={
-        'latitude': 37.76,
-        'longitude': -122.4,
-        'zoom': 11,
-        'pitch': 50,
-    },
-    layers=[{
-        'type': 'HexagonLayer',
-        'data': df,
-        'radius': 200,
-        'elevationScale': 4,
-        'elevationRange': [0, 1000],
-        'pickable': True,
-        'extruded': True,
-    }, {
-        'type': 'ScatterplotLayer',
-        'data': df,
-    }]
-)
