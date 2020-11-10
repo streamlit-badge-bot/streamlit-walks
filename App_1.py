4289a920b0db0d85c755a8d8be688aa05d0df0ca
Pip <70948349+pippinstall@users.noreply.github.com>
2020-11-10 15:18:31 +0000

    Update App_1.py

--- App_1.py
+++ App_1.py
@@ -73,7 +73,7 @@ section = df['Section'].unique()
 filter_section = st.selectbox("Section", (section))
 
 minim = np.min(df['Height (m)'])
-minim
+minim.type()
 
 maxim = np.max(df['Height (m)'])
 maxim
