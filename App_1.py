4f551e1a6b0977f7e09c8020eb6f485a4b6b6838
Pip <70948349+pippinstall@users.noreply.github.com>
2020-11-10 09:23:06 +0000

    Update App_1.py

--- App_1.py
+++ App_1.py
@@ -18,7 +18,8 @@ st.markdown('<style>h3{color: black; text-align:center;}</style>', unsafe_allow_
 # --------------------------------
 
 st.title("Wainwrights: Which one should I climb?")
-st.markdown("Wainwrights are the 214 English peaks mapped out in Alfred Wainwright's Pictorial Guide to the Lakeland Fells (1955–66). It is popular for walkers to use these routes when climbing a fell in the Lake District. However, with so much choice, how can we narrow down which one to walk up? The purpose of this app is to narrow down the Wainwrights to aid in your next fell climb in the Lake District.")
+st.header("Wainwrights are the 214 English peaks mapped out in Alfred Wainwright's Pictorial Guide to the Lakeland Fells (1955–66).")
+st.markdown("The purpose of this app is to narrow down the Wainwrights to aid in your next fell climb in the Lake District.")
 
 # --------------------------------
 # Import Data
