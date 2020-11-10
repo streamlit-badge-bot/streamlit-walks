0a55742784dcf99778ab60cc945f234a8c4e9c72
Pip <70948349+pippinstall@users.noreply.github.com>
2020-11-10 16:48:06 +0000

    Update App_1.py

--- App_1.py
+++ App_1.py
@@ -45,6 +45,7 @@ st.sidebar.title("Filter")
 heights = st.sidebar.slider('Select a height (m)', int(df['Height (m)'].min()), int(df['Height (m)'].max()), (int(df['Height (m)'].min()), int(df['Height (m)'].max())))
 section = df['Section'].unique()
 filter_section = st.sidebar.selectbox("Section", (section))
+filter_section
 
 fig = px.scatter_mapbox(df[(df['Height (m)'] >= heights[0]) & (df['Height (m)'] <= heights[1])],
                         lat = "Latitude",
