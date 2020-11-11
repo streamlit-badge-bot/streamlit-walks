47a4d897b29af561d8edec5a649491c6184acf08
Pip <70948349+pippinstall@users.noreply.github.com>
2020-11-11 11:14:31 +0000

    Update App_1.py

--- App_1.py
+++ App_1.py
@@ -59,7 +59,7 @@ names = selected['Name'].unique()
 names = sorted(names, reverse = False)
 
 for i in names:
-    exclude_peaks = st.sidebar.checkbox(i)
+    exclude_peaks = st.sidebar.checkbox("Exclude peaks:", i)
 if exclude_peaks:
     st.write('Excluded peaks')
     
